8ecfd1bb6382386f552da89a2fda609ec53ac4f1
Chris William <baunachkrysztof@gmail.com>
2022-04-19 11:39:26 -0500

    fix failing test

--- src/App.test.ts
+++ src/App.test.ts
@@ -4,9 +4,9 @@ it('verify basic addition', () => {
   expect(5 + 2).toBe(7)
 });
 
-it('verify type check failure', () => {
-  const a: string = 5
-  expect(a).toBe(5)
+it('verify type check success', () => {
+  const a: string = "5"
+  expect(a).toBe("5")
 });
 
 export {}
